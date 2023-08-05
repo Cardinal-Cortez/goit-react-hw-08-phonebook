122f1726c01862e33cdb4e5d430178cdb9bb6128
Illia Bidiuk <Core778@gmail.com>
2023-08-05 23:08:57 +0300

    second commit

--- src/components/InputNumber/InputNumber.jsx
+++ src/components/InputNumber/InputNumber.jsx
@@ -10,7 +10,7 @@ export const InputNumber = ({handleNumberChange, number}) => {
                 value={number}
                 type="tel"
                 name="number"
-                pattern= "^(\\+?[0-9.\\(\\)\\-\\s\]*)$"
+                pattern= "\\+?\\d{1,4}?[ .\\-\\s\]?\\(?\\d{1,3}?\\)?[ .\\-\\s\]?\\d{1,4}[ .\\-\\s\]?\\d{1,4}[ .\\-\\s\]?\\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
             />
